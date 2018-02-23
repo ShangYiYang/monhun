e8e84305e2e1ae2327696a9212fe143f486a2705
ShangYiYang <shangyang824@gmail.com>
2018-02-22 22:22:09 -0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div ref="app" id="app">
+    <ul>
+      <li><div><router-link to="/"> Main </router-link></div></li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    toggleSideBarOn: function() {
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  body{
+    margin: 0px;
+  }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: beige;
+  }
+
+  li {
+    float: left;
+  }
+
+  li div {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover {
+    background-color: white;
+  }
+  #toggleOn {
+    background-color: mediumpurple;
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="hello">
+    <div id="container">
+      <div ref="sidebar" id="sidebar" v-on:click="toggleSideBarOff">
+        <div ref="toggleOff" id="toggleOff"></div>
+        <div id="search"></div>
+        <p> This is the sidebar </p>
+      </div>
+      <label ref="toggleOn" id="toggleOn" v-on:click="toggleSideBarOn">☰</label>
+      <div ref="content" id="content">
+        <div></div>
+        <router-view/>
+        <div id="armor">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div id="info">
+          <h1>This is where the info goes</h1>
+        </div>
+        <div id="test"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  methods: {
+    toggleSideBarOn: function() {
+      this.$refs["sidebar"].style.marginLeft = "0px";
+    },
+    toggleSideBarOff: function() {
+      this.$refs["sidebar"].style.marginLeft = "-250px";
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #container {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  #container {
+    display: flex;
+  }
+
+  #toggleOn {
+    width: 25px;
+    height: 25px;
+    display: inline-block;
+    background-color: aqua;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  #toggleOff {
+    width: 35px;
+    height: 35px;
+    background-color: aqua;
+    display: inline-block;
+  }
+
+  #search {
+    height: 35px;
+    width: 75%;
+    background-color: black;
+    display: inline-block;
+  }
+
+  #sidebar {
+    height: 100vh;
+    background-color:gray;
+    width: 250px;
+    transition: all 1s;
+    /*left: -25%;*/
+    margin-left: -250px;
+  }
+
+  #armor {
+    height: 50px;
+    width: 625px;
+    padding-top: 25px;
+  }
+
+  #armor div {
+    width: 50px;
+    height: 50px;
+    background-color: black;
+    display: inline-block;
+    margin-left: 25px;
+  }
+
+  #info {
+    margin-left: 25px;
+  }
+
+  #test {
+    height: 100px;
+    width: 100px;
+    background-color: #009926;
+    transition: width 2s;
+  }
+
+  #test:hover {
+    width: 300px;
+  }
+</style>
